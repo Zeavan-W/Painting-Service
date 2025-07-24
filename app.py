e2af0a3f60c8eea999362de4b100e8dbdf2e3976
Zeavan-W <wztyfzheng@gmail.com>
2025-07-24 18:31:10 +0800

    Add files via upload

--- app.py
+++ app.py
@@ -1,20 +1,30 @@
+import os
 from flask import Flask, render_template_string, request, redirect, flash
 from flask_mail import Mail, Message
+from flask_wtf import FlaskForm, CSRFProtect
+from wtforms import StringField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # 用于闪现消息
+app.secret_key = os.environ.get('SECRET_KEY', 'your_secret_key')
+csrf = CSRFProtect(app)
 
-# 邮箱配置（以QQ邮箱为例，其他邮箱请修改对应参数）
-app.config['MAIL_SERVER'] = 'smtp.qq.com'
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USE_SSL'] = True
-app.config['MAIL_USERNAME'] = '735433470@qq.com'
-app.config['MAIL_PASSWORD'] = 'sfndnqpaifpxbeff'
-app.config['MAIL_DEFAULT_SENDER'] = '735433470@qq.com'
+# 邮箱配置（用环境变量保护敏感信息）
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER', 'smtp.qq.com')
+app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 465))
+app.config['MAIL_USE_SSL'] = os.environ.get('MAIL_USE_SSL', 'True') == 'True'
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', '735433470@qq.com')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', 'sfndnqpaifpxbeff')
+app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER', '735433470@qq.com')
 
 mail = Mail(app)
 
-# 直接用字符串渲染表单页面，方便初学者测试
+class ContactForm(FlaskForm):
+    name = StringField('Name', validators=[DataRequired(), Length(max=30)])
+    phone = StringField('Phone', validators=[DataRequired(), Length(max=20)])
+    address = StringField('Address', validators=[DataRequired(), Length(max=100)])
+    message = TextAreaField('Message', validators=[DataRequired(), Length(max=500)])
+
 HTML = '''
 <!DOCTYPE html>
 <html lang="zh-CN">
@@ -35,10 +45,11 @@ HTML = '''
     {% endif %}
   {% endwith %}
   <form action="/" method="POST">
-    <input type="text" name="name" placeholder="您的姓名" required><br>
-    <input type="tel" name="phone" placeholder="联系电话" required><br>
-    <input type="text" name="address" placeholder="服务地址" required><br>
-    <textarea name="message" placeholder="需求描述" required></textarea><br>
+    {{ form.hidden_tag() }}
+    <input type="text" name="name" placeholder="您的姓名" value="{{ form.name.data or '' }}" maxlength="30" required><br>
+    <input type="tel" name="phone" placeholder="联系电话" value="{{ form.phone.data or '' }}" maxlength="20" required><br>
+    <input type="text" name="address" placeholder="服务地址" value="{{ form.address.data or '' }}" maxlength="100" required><br>
+    <textarea name="message" placeholder="需求描述" maxlength="500" required>{{ form.message.data or '' }}</textarea><br>
     <button type="submit">提交</button>
   </form>
 </body>
@@ -47,31 +58,31 @@ HTML = '''
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'POST':
-        name = request.form.get('name')
-        phone = request.form.get('phone')
-        address = request.form.get('address')
-        message = request.form.get('message')
+    form = ContactForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        phone = form.phone.data
+        address = form.address.data
+        message = form.message.data
 
-        # 构建邮件内容
         email_body = f"""
         姓名: {name}
         电话: {phone}
         地址: {address}
         需求描述: {message}
         """
-
-        # 发送邮件
         msg = Message(subject="网站表单新提交",
-                      recipients=['735433470@qq.com'],
+                      recipients=[os.environ.get('MAIL_RECEIVER', '735433470@qq.com')],
                       body=email_body)
         try:
             mail.send(msg)
             flash('提交成功，感谢您的反馈！')
-        except Exception as e:
+        except Exception:
             flash('邮件发送失败，请稍后再试。')
         return redirect('/')
-    return render_template_string(HTML)
+    elif request.method == 'POST':
+        flash('表单填写有误，请检查后再提交。')
+    return render_template_string(HTML, form=form)
 
 if __name__ == '__main__':
-    app.run(debug=True) 
+    app.run(debug=False) 
